003cd8a690246434805a9c5a5bfa45151717b334
cfarmer-fearless <cfarmer@fearless.tech>
2022-06-07 23:19:17 -0400

    rsvp page

--- pages/rsvp.tsx
+++ pages/rsvp.tsx
@@ -1,7 +1,40 @@
-import { NextPage } from "next";
+import { GetStaticProps, NextPage } from "next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useTranslation } from "react-i18next";
 
 const RSVP: NextPage = () => {
-  return <div>RSVP Page</div>;
+  const { t } = useTranslation("common");
+
+  return (
+    <main>
+      <div
+        className="parallax-container"
+        style={{ backgroundImage: "url(/garden3.jpg)", height: "100vh" }}
+      >
+        <div className="abs-center-container">
+          <div className="abs-center-content">
+            <h3>{t("rsvpIntro")}</h3>
+            <a
+              className="link-button"
+              href="https://docs.google.com/forms/d/1zgR0WWdiu1pgyZjOMnMbNLiUgDu1vSvIDEInEwJ3a4A/edit"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {t("rsvpLink")}
+            </a>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export const getStaticProps: GetStaticProps = async ({ locale = "en-US" }) => {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["common"])),
+    },
+  };
 };
 
 export default RSVP;
