831b618d6a1294a659ebbc1ffd286b4536225231
cfarmer-fearless <cfarmer@fearless.tech>
2022-06-19 13:23:21 -0400

    umami script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,18 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { appWithTranslation } from "next-i18next";
 import { Navbar } from "../components/Navbar";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      {process.env.UMAMI_SCRIPT_URL && process.env.UMAMI_WEBSITE_ID && (
+        <Script
+          src={process.env.UMAMI_SCRIPT_URL}
+          data-website-id={process.env.UMAMI_WEBSITE_ID}
+          strategy="lazyOnload"
+        />
+      )}
       <Navbar />
       <Component {...pageProps} />
     </>
