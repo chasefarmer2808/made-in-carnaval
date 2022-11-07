c08b6f96f718ab97fe74d385453ad5f7d57af1d9
chasefarmer2808 <chasefarmer94@gmail.com>
2022-11-07 14:49:16 -0500

    fix locale path error

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,10 @@
+const path = require("path");
+
 module.exports = {
   i18n: {
     locales: ["en-US", "pt-BR"],
     defaultLocale: "en-US",
     domains: [{ domain: "madeinrio2023.com", defaultLocale: "en-US" }],
+    localePath: path.resolve("./public/static/locales"),
   },
 };
