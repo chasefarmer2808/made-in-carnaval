14a0717e4d7bba3d84da7f74e3ab4530648e8c5c
cfarmer-fearless <cfarmer@fearless.tech>
2023-01-25 22:27:02 -0500

    fix ceremony page

--- pages/ceremony.tsx
+++ pages/ceremony.tsx
@@ -12,52 +12,48 @@ const Ceremony: NextPage = () => {
       <div className={`${styles["banner-container"]} parallax-container`}>
         <div className="abs-center-container" style={{ margin: "16px" }}>
           <div className={`${styles["ceremony-content"]} abs-center-content`}>
-            <h1>{t("ceremonyPageTitle")}</h1>
-            <div className={styles["ceremony-content-grid"]}>
-              <div className={styles["ceremony-content-label"]}>
-                {t("when")}:
-              </div>
-              <div>April 15, 2023</div>
-              <div className={styles["ceremony-content-label"]}>
-                {t("where")}:
-              </div>
-              <div>
-                <span>Cais do Oriente</span>
-                <br></br>
-                <span>Rua Visonde de Itaborai, 8</span>
-                <br></br>
-                <span>Centro, Rio de Janeiro</span>
-                <br></br>
-                <div>
-                  <a
-                    className="inline"
-                    style={{ marginRight: "8px" }}
-                    rel="noreferrer"
-                    target="_blank"
-                    href="https://www.google.com/maps/place/Cais+do+Oriente/@-22.9013405,-43.1755447,15z/data=!4m5!3m4!1s0x0:0x66b71b30656c1ee5!8m2!3d-22.9013405!4d-43.1755447"
-                  >
-                    {t("map")} <Icon name="external-link" />
-                  </a>
-                  <a
-                    className="inline"
-                    rel="noreferrer"
-                    target="_blank"
-                    href="https://caisdooriente-rj.com.br/"
-                  >
-                    {t("website")} <Icon name="external-link" />
-                  </a>
-                </div>
-              </div>
-              <div className={styles["ceremony-content-label"]}>
-                {t("details")}:
-              </div>
-              <div>{t("detailsInfo")}</div>
-              <div className={styles["ceremony-content-label"]}>
-                {t("attire")}:
-              </div>
-              <div>{t("attireInfo")}</div>
+            <h3>{t("ceremonyPageTitle")}</h3>
+          </div>
+        </div>
+      </div>
+      <div className="content-stripe">
+        <div className={styles["ceremony-content-grid"]}>
+          <div className={styles["ceremony-content-label"]}>{t("when")}:</div>
+          <div>April 15, 2023</div>
+          <div className={styles["ceremony-content-label"]}>{t("where")}:</div>
+          <div>
+            <span>Cais do Oriente</span>
+            <br></br>
+            <span>Rua Visonde de Itaborai, 8</span>
+            <br></br>
+            <span>Centro, Rio de Janeiro</span>
+            <br></br>
+            <div>
+              <a
+                className="inline"
+                style={{ marginRight: "8px" }}
+                rel="noreferrer"
+                target="_blank"
+                href="https://www.google.com/maps/place/Cais+do+Oriente/@-22.9013405,-43.1755447,15z/data=!4m5!3m4!1s0x0:0x66b71b30656c1ee5!8m2!3d-22.9013405!4d-43.1755447"
+              >
+                {t("map")} <Icon name="external-link" />
+              </a>
+              <a
+                className="inline"
+                rel="noreferrer"
+                target="_blank"
+                href="https://caisdooriente-rj.com.br/"
+              >
+                {t("website")} <Icon name="external-link" />
+              </a>
             </div>
           </div>
+          <div className={styles["ceremony-content-label"]}>
+            {t("details")}:
+          </div>
+          <div>{t("detailsInfo")}</div>
+          <div className={styles["ceremony-content-label"]}>{t("attire")}:</div>
+          <div>{t("attireInfo")}</div>
         </div>
       </div>
     </main>
